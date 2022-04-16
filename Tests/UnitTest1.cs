46cbd2418f53d6fab816c66ceba516b351deef43
Максим Дианов <shot_play@mail.ru>
2022-04-16 12:39:33 +0300

    prikol

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Library;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int One = 14;
+            int Two = 34;
+            string Result = Code.Sum(One, Two);
+            Assert.AreEqual("Сумма равна 48", Result);
+        }
+    }
+}
---
+++ Tests1604/Pages/Calck.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Library;
+
+namespace Tests1604.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Calck.xaml
+    /// </summary>
+    public partial class Calck : Page
+    {
+        public Calck()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int OneResult = int.Parse(CalculateTexBoxOne.Text);
+            int TwoResult = int.Parse(CalculateTexBoxTwo.Text);
+            ResultCalculate.Text = Code.Sum(OneResult, TwoResult);
+        }
+    }
+}
